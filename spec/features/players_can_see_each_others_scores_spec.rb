f52c9cded2978cf2bfa81ed5cf014a6d033e5495
Alexa <alexakearns@outlook.com>
2020-05-17 16:14:58 +0100

    web helper created for sign in and play

--- spec/features/players_can_see_each_others_scores_spec.rb
+++ spec/features/players_can_see_each_others_scores_spec.rb
@@ -1,9 +1,7 @@
 feature 'players can see each others scores' do
   scenario 'players can see each others scores' do
     visit '/'
-    fill_in 'player_1', with: "Nigel"
-    fill_in 'player_2', with: "Colin"
-    click_on 'ENTER'
+    sign_in_and_play
     click_on 'START GAME'
     expect(page).to have_content 'Nigel - 100'
     expect(page).to have_content 'Colin - 100'
