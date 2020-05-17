3327306f0274fdd58081b3bca37b9d1090f087d5
Alexa <alexakearns@outlook.com>
2020-05-17 17:47:27 +0100

    feature test inc. not have. Attack unit test - Red

--- spec/features/player_attack_reduces_oppo_hp_spec.rb
+++ spec/features/player_attack_reduces_oppo_hp_spec.rb
@@ -3,6 +3,7 @@ feature 'attack reduces opposition hit points by 10' do
     sign_in_and_play
     click_on 'START GAME'
     click_on 'ATTACK'
+    expect(page).not_to have_content 'Colin - 100 HP'
     expect(page).to have_content 'Colin - 90 HP'
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,6 +2,7 @@ require 'player'
 
 describe Player do
   let(:john) { Player.new('John') }
+  let(:derek) { Player.new('Derek') }
 
   describe '#name' do
     it 'returns the players name that is entered' do
@@ -12,6 +13,13 @@ describe Player do
   describe '#hit points' do
     it 'default hit points for new player is 100' do
       expect(john.hit_points).to eq 100
+    end
+  end
+
+  describe '#attack' do
+    it 'reduces opposition points by 10' do
+      john.attack(derek)
+      expect(derek.hit_points).to eq 90
     end
   end
 end
