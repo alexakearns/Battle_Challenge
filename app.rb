59385d506e3934e61b7c818c7a1d32a9bda21484
Alexa <alexakearns@outlook.com>
2020-05-17 16:28:23 +0100

    /attack changed from post to get route

--- app.rb
+++ app.rb
@@ -25,7 +25,7 @@ class Battle < Sinatra::Base
     erb :game
   end
 
-  post '/attack' do
+  get '/attack' do
     @player_1 = session[:player_1]
     @player_2 = session[:player_2]
     erb :attack
